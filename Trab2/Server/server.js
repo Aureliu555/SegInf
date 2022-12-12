186a1b307b1b5f95f8545d339373fd4747295913
Aureliu555 <A48285@alunos.isel.pt>
2022-12-12 14:00:17 +0000

    server initialized

--- Trab2/Server/server.js
+++ Trab2/Server/server.js
@@ -3,29 +3,28 @@ const https = require("https");
 // Handling GET request (npm install express)
 const express = require("express");
 // Load of files from the local file system
-var fs = require('fs'); 
+const fs = require('fs'); 
 
 const PORT = 4433;
 const app = express();
 
 // Get request for resource /
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
     console.log(
         req.socket.remoteAddress
         //+ ' ' + req.socket.getPeerCertificate().subject.CN
         + ' ' + req.method
         + ' ' + req.url);
     res.send("<html><body>Secure Hello World with node.js</body></html>");
-});
-
+})
 
 // configure TLS handshake
 const options = {
     key: fs.readFileSync('./keyStore.pem'),
     cert: fs.readFileSync('./certificate.pem'),
     //ca: fs.readFileSync('<server trustbase PEM (root CA)>'), 
-    requestCert: true, 
-    rejectUnauthorized: true
+    requestCert: false, 
+    rejectUnauthorized: false
 };
 
 // Create HTTPS server
