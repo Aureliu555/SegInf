1452bfc6809837fc9bcd919b6f8823127e2c90e6
Jose_ <joseamsilva100@gmail.com>
2022-12-20 22:09:21 +0000

    Functional but not completed

--- Trab2/Exercise7/tasks-api.mjs
+++ Trab2/Exercise7/tasks-api.mjs
@@ -3,20 +3,23 @@ import cookieParser from 'cookie-parser';
 import webFunctions from './tasks-web.mjs';
 
 
-
-//----------------------------- CONSTANTS DEFINITIONS -----------------------------------
 const web = webFunctions();
 const port = 3001
 const CALLBACK = 'home'
 const app = express()
 
-
+app.set('views', 'views')
+app.set('view engine', 'hbs')
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+app.use(express.static('public'))
 app.use(cookieParser());
 app.get('/', web.login)
 app.get('/login', web.loginForm)
 app.get('/'+CALLBACK, web.home)
 app.get('/tasks', web.getTasks)
-//app.post('/tasks', web.addTasks) 
+app.get('/insertTask', web.insertTaskForm)
+app.post('/insertTask', web.addTasks) 
 
 app.listen(port, (err) => {
     if (err) return console.log('something bad happened')
--- Trab2/Exercise7/tasks-web.mjs
+++ Trab2/Exercise7/tasks-web.mjs
@@ -4,27 +4,29 @@ import FormData from 'form-data';
 import jwt from 'jsonwebtoken';
 import {newEnforcer} from 'casbin';
 
-//-----------------------------------------------------------------------------------------------------------------
+
 const CLIENT_ID = '725975013772-2gbsl365ntj46gode0hq2jsq1c9vvfbk.apps.googleusercontent.com'
 const CLIENT_SECRET = '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'
 const API_KEY = 'AIzaSyB8Oz7v_H-Ak8-TfDhUBhyzvYUL0OAfo1U'
 const CALLBACK = 'home'
 const e = await newEnforcer("./rbac/basic_model.conf", "./rbac/basic_policy.csv");
-const TASKS = {
+/* const TASKS = {
     "kind": "Trivial",
     "items": [
         {"kind": "Urgent", "title": "Wash the dishes", "status": "undone"},
         {"kind": "Not important", "title": "Study more", "status": "undone"}
     ]
-  }
+  } */
 
-//-----------------------------------------------------------------------------------------------------------------
+
 export default function webFunctions(){
     return {
         login: login,
         loginForm: loginForm,
         getTasks: getTasks,
         home: home,
+        insertTaskForm: insertTaskForm,
+        addTasks: addTasks
 
     }
 
@@ -39,21 +41,37 @@ export default function webFunctions(){
 }
 
     function getTasks(req, rsp){
-        axios.get(`https://tasks.googleapis.com/tasks/v1/users/@me/lists?access_token=${req.cookies.ACCESS_TOKEN}&key=${API_KEY}`,
-         {headers: { Authorization: 'Bearer ' + req.cookies.ACCESS_TOKEN, Accept: 'application/json' }})
+            axios.get(`https://tasks.googleapis.com/tasks/v1/lists/${req.cookies.TASKLIST_ID}/tasks`, {headers: { Authorization: 'Bearer ' + req.cookies.ACCESS_TOKEN }})
         .then(function (resp){
-            resp.render('/tasks')
+                let str = ""
+                resp.data.items.forEach((item, idx) => {
+                    str +=  `<div>${idx+1}. ${item.title} </div>` 
+                });
+                rsp.send(str + '<div> <a href="/insertTask"> Add new </a> </div>')
         })
         .catch(function (err){
             rsp.send(err)
         })
     }
 
-/*     async function addTasks(tasks, newTask){     
-        tasks.items.push(newTask)
-        return Promise.resolve(newTask)
+    async function insertTaskData(taskName) {
+        return taskName
     } 
- */
+
+
+    function insertTaskForm(req, rsp) {
+        rsp.render('insertTask', {})
+    }
+
+    function addTasks(req, rsp){
+        if(!e.enforce(req.cookies.USER_EMAIL, "tasks", "free")){
+            axios.post(`https://tasks.googleapis.com/tasks/v1/lists/${req.cookies.TASKLIST_ID}/tasks`, {headers: { Authorization: 'Bearer ' + req.cookies.ACCESS_TOKEN }})
+            .then(function(resp){
+                
+            })}
+        rsp.status(302).redirect('/getPremium')
+    } 
+
     function login (req, rsp) { 
         rsp.send('<a href=/login>Use Google Account</a>')
     }
@@ -62,33 +80,24 @@ export default function webFunctions(){
         rsp.redirect(302,
             'https://accounts.google.com/o/oauth2/v2/auth?'
             + 'client_id='+ CLIENT_ID +'&'
-            + 'scope=openid%20email&'
-            + 'state=value-based-on-user-session&'
+            + 'scope=openid%20email%20https://www.googleapis.com/auth/tasks&'
+            //+ 'state=value-based-on-user-session&'
             + 'response_type=code&'
             + 'redirect_uri=http://localhost:3001/'+CALLBACK)
     }
     function home(req, rsp) {
-        //
-        // TODO: check if 'state' is correct for this session
-        //
-        //console.log('making request to token endpoint')
-    
         const form = new FormData();
+        var jwt_payload;
+        var role;
         form.append('code', req.query.code);
         form.append('client_id', CLIENT_ID);
         form.append('client_secret', CLIENT_SECRET);
         form.append('redirect_uri', 'http://localhost:3001/'+CALLBACK);
         form.append('grant_type', 'authorization_code');
         axios.post('https://www.googleapis.com/oauth2/v3/token', 
-            // body parameters in form url encoded
-            form,
-            { headers: form.getHeaders() }, 
-          )
+            form, { headers: form.getHeaders() }, )
           .then(async function (response) {
-            var jwt_payload = jwt.decode(response.data.id_token)
-            //console.log(jwt_payload)
-            //const email = jwt_payload.email
-            //console.log('AVAILABLE EMAIL:', email)
+            jwt_payload = jwt.decode(response.data.id_token)
     
             // a simple cookie example
             //console.log("ACCESS TOKEN ------->", response.data.access_token)
@@ -97,8 +106,15 @@ export default function webFunctions(){
             rsp.cookie("BEARER_TOKEN", response.data.id_token)
             rsp.cookie("USER_EMAIL", jwt_payload.email)
 
-            let role = await checkRole(jwt_payload.email)
-            // HTML response with the code and access token received from the authorization server
+            role = await checkRole(jwt_payload.email)
+          })
+          .catch(
+            function (error) {
+                rsp.send(error)
+          });
+          axios.get(`https://tasks.googleapis.com/tasks/v1/users/@me/lists`, {headers: { Authorization: 'Bearer ' + req.cookies.ACCESS_TOKEN }})
+          .then(function (resp){
+            rsp.cookie("TASKLIST_ID", resp.data.items[0].id)
             rsp.send(
                 '<div> Hi <b>' + jwt_payload.email + '</b> </div><br>' +
                 '<div> <a>User Status: '+ role +' </a> </div>'+
@@ -106,13 +122,12 @@ export default function webFunctions(){
                 'Go back to <a href="/">Home screen</a>'
             );
           })
-          .catch(
-            function (error) {
-            //console.log(error)
-                rsp.send(error)
-          });
-    }
-
+          .catch(function (error){
+                console.log(error)   
+              }
+          )}
 }
+
+
 
 
